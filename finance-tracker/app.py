e339dfc4abb03226552b084bbd4e72223705139f
Nitesh Kumar <niteshkr9155166021@gmail.com>
2025-01-18 19:16:59 +0530

    added fetch data from add expenses

--- finance-tracker/app.py
+++ finance-tracker/app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
+expenses = []
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', expences=expenses)
 
 
-@app.route('/add-expense')
+@app.route('/add-expense', methods=['GET', 'POST'])
 def add_expense():
-    return render_template('add_expense.html')
+        if request.method == 'POST':
+             amount = request.form['amount']
+             category = request.form['category']
+             description = request.form['description']
+             date = request.form['date']
+
+             expense = {
+                  'amount': amount,
+                  'category': category,
+                  'description': description,
+                  'date':date
+             }
+             expenses.append(expense)
+             return redirect(url_for('index'))
+        return render_template('add_expense.html')
 
 
 if __name__ == '__main__':
