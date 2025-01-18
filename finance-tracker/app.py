8e9f63165bef92167e3b6c1488945340249d32b6
Nitesh Kumar <niteshkr9155166021@gmail.com>
2025-01-18 23:53:07 +0530

    added budget adding functionality

--- finance-tracker/app.py
+++ finance-tracker/app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 
 expenses = []
+total_budget = 10000
 
 @app.route('/')
 def index():
-    return render_template('index.html', expences=expenses)
+    total_expenses = sum([float(expense["amount"]) for expense in expenses])
+    remaining_balance = total_budget - total_expenses
+    return render_template('index.html', expenses=expenses, total_budget=total_budget, remaining_balance= remaining_balance)
 
 
 @app.route('/add-expense', methods=['GET', 'POST'])
@@ -28,6 +31,16 @@ def add_expense():
              return redirect(url_for('index'))
         return render_template('add_expense.html')
 
+@app.route('/set-budget', methods=['GET', 'POST'])
+def set_budget():
+     global total_budget
+
+     if request.method == 'POST':
+          new_budget = request.form.get('budget')
+          if new_budget and new_budget.isdigit():
+               total_budget = float(new_budget)
+               return redirect('/')
+     return render_template('set_budget.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
